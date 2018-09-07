b2ab5b53ad3c5abffc2d3e219e5b05b2d3b037fb
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-09-07 19:44:17 +0400

    more btn

---
+++ js/parts/more-styles.js
@@ -0,0 +1,12 @@
+let btnStyles = document.querySelector('.button-styles'),
+	boxMore = document.getElementsByClassName('styles-2');
+
+btnStyles.addEventListener('click', function() {
+
+	this.style.display = 'none';
+
+	for (let i = 0; i < boxMore.length; i++) {
+		boxMore[i].classList.remove('hidden-lg', 'hidden-md', 'hidden-sm', 'hidden-xs');
+		boxMore[i].classList.add('col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+	}
+});
