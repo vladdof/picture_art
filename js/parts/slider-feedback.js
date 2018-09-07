5c2be7f16cd370235c6bc62b4a5b0df7be307d13
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-09-07 18:32:12 +0400

    slider bottom ready

--- js/parts/slider-feedback.js
+++ js/parts/slider-feedback.js
@@ -1,6 +1,19 @@
 // slider feedback
-let slideIndex = 1,
-	slidesF = document.getElementsByClassName('feedback-slider-item'),
+
+function nextAuto() {
+	plusSlides(1);
+}
+
+window.onload = function() { // start slides before load
+    showSlidesF();
+    setInterval(function() { 
+        nextAuto();
+    },5000);
+};
+
+let slideIndex = 0,
+	boxSlider = document.querySelector('.feedback-slider-box'),
+	slidesF = document.getElementsByClassName('feedback-slider-item').length,
 	prevF = document.querySelector('.main-prev-btn'),
 	nextF = document.querySelector('.main-next-btn');
 
@@ -9,21 +22,18 @@ showSlidesF(slideIndex);
 function showSlidesF(n) {
 
 	// return to first after scrolling to last
-	if (n > slidesF.length) {
-		slideIndex = 1;
+	if (n > slidesF-1) {
+		slideIndex = 0;
 	};
 
-	if (n < 1) {
-		slideIndex = slidesF.length;
+	if (n < 0) {
+		slideIndex = slidesF - 1;
 	};
 
-	// Hide all slides, only one
-	for (let i = 0; i < slidesF.length; i++) {
-		// slidesF[i].style.display = 'none';
-		slidesF[i].style.display = 'none';
-	}
+	let boxWidth = boxSlider.clientWidth,
+		offset = -slideIndex*boxWidth;
 
-	slidesF[slideIndex - 1].style.display = 'flex';
+	boxSlider.setAttribute('style', `transform: translate(${offset}px)`);
 }
 
 function plusSlides(n) {
@@ -36,14 +46,3 @@ prevF.addEventListener('click', ()=> {
 nextF.addEventListener('click', ()=> {
 	plusSlides(1);
 });
-
-function nextAuto() {
-	plusSlides(1);
-}
-
-// window.onload = function() { // start slides before load
-//     showSlidesF();
-//     setInterval(function() { 
-//         nextAuto();
-//     },5000);
-// };
