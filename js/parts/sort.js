67a67671c50534cc6949f655a22e272c97a279bf
Vladislav Efanov <vladislav.efanov@gmail.com>
2018-09-10 16:35:13 +0400

    sort

--- js/parts/sort.js
+++ js/parts/sort.js
@@ -11,30 +11,31 @@ function sort() {
 		for (let i = 0; i < linkItem.length; i++) {
 
 			if (linkItem[i].classList.contains(target)) {
-
 				linkItem[i].classList.add('active');
-				
-			}	else if (target == 'active') {
 
-				console.log('new new new');
-			}	else {
-					linkItem[i].classList.remove('active');	
+			  	for (let j=0; j<portfolio.length; j++) {
+
+				  	if (portfolio[j].classList.contains(target)) {
+				    	portfolio[j].style.display = 'block';
+
+				    } else if (target == 'grandmother' || target == 'granddad') {
+						no.style.display = 'block';
+						portfolio[j].style.display = 'none';
+
+					}	else {
+				    	portfolio[j].style.display = 'none';
+				    	no.style.display = 'none';
+				    }
+				}
+
+			} else {
+				if (target.match(/\bactive\b/)) {
+					break;
+				} else {
+					linkItem[i].classList.remove('active');
+				}
+					
 			}
-		}
-
-	  	for (let j=0; j<portfolio.length; j++) {
-
-		  	if (portfolio[j].classList.contains(target)) {
-		    	portfolio[j].style.display = 'block';
-
-		    } else if (target == 'grandmother' || target == 'granddad') {
-				no.style.display = 'block';
-				portfolio[j].style.display = 'none';
-
-			}	else {
-		    	portfolio[j].style.display = 'none';
-		    	no.style.display = 'none';
-		    }
 		}
 	});
 }
